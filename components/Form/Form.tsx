096697acd0a982d81bacbba579b07732b4eb5c20
BrunoCampxs <bruno.andrade115@etec.sp.gov.br>
2024-09-26 22:09:59 -0300

    Segundo Commit

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,31 +1,51 @@
-import React, {useState} from "react";
-//import {useState } from "react-native-gesture-handler";
-import {Pressable, Text, TextInput ,View } from "react-native";
-export function Form()
+import React, { useState } from "react";
+import { Pressable, Text, TextInput, View } from "react-native";
+
+export function Form() {
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [imc, setImc] = useState('');
+    
+
+function imcCalulator()
 {
-    const [side, setSide] = useState(0);
-    const [area, setArea] = useState(0);
-    function calculateArea(){
-     
-        const result = ((side ** 2)* Math.sqrt(3)) / 4;
-        setArea(result)
-    }
-    return
-    (
+    let totalImc = (weight/(height*height)).toFixed(2)
+    setImc(totalImc)
+
+}
+    function validatorImc ()
+{
+   if(!weight  && height !='') //*negação
+    {
+     imcCalulator()
+     setHeight('')
+     setWeigh('')
+
+   } 
+}
+    return (
         <View>
             <View>
-            <Text>Lado:</Text>
-            <TextInput
-            onChangeText={setSide}
-            placeholder="Digite o lado em cm"
-            inputMode="numeric"
-            value="side"
-            />
-            <Pressable
-             title="calcular"
-             onPress={ () => calculateArea() }
-            />
+                <Text>Altura:</Text>
+                <TextInput
+                    placeholder="Ex. 1.75"
+                    value={height} 
+                 
+                />
+                
+                <Text>Peso:</Text>
+                <TextInput
+                    placeholder="Ex. 67.5"
+                    value={weight} 
+                   
+                />
+
+                <Pressable
+                onPress={() => validatorImc()}>
+                    <Text>Calcular</Text>
+                </Pressable>
+                <Text>{imc}</Text>
             </View>
         </View>
-    )
+    );
 }
--- components/Secundario.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text, View } from "react-native-reanimated/lib/typescript/Animated";
-
-export function Secundario ()
-{
-    <View>
-        <Text>MXXV</Text>
-    </View>
-}
